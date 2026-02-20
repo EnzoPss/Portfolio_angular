a14476d204272533e4b73accdc0e8d51b4df267b
EnzoPss <enzopousse.06@gmail.com>
2026-02-20 15:15:59 +0100

    save

---
+++ src/app/components/parcours/parcours.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parcours',
+  imports: [],
+  templateUrl: './parcours.html',
+  styleUrl: './parcours.css',
+})
+export class Parcours {
+
+}
