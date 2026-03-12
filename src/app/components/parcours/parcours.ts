888dd954ce2405cd0fd500d49032f1708f601387
EnzoPss <enzopousse.06@gmail.com>
2026-03-12 09:21:19 +0100

    patch visuel

--- src/app/components/parcours/parcours.ts
+++ src/app/components/parcours/parcours.ts
@@ -1,16 +1,29 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, TemplateRef } from '@angular/core';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-parcours',
-  imports: [],
+  imports: [CommonModule, NgTemplateOutlet],
   templateUrl: './parcours.html',
   styleUrl: './parcours.css',
 })
 export class Parcours implements AfterViewInit {
 
+  activeTemplate: TemplateRef<any> | null = null;
+
+  openModal(template: TemplateRef<any>, event: Event): void {
+    event.preventDefault();
+    this.activeTemplate = template;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeModal(): void {
+    this.activeTemplate = null;
+    document.body.style.overflow = '';
+  }
+
   ngAfterViewInit(): void {
     const items = document.querySelectorAll('.timeline-item');
-
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -18,8 +31,6 @@ export class Parcours implements AfterViewInit {
         }
       });
     }, { threshold: 0.3 });
-
     items.forEach(item => observer.observe(item));
   }
-
 }
